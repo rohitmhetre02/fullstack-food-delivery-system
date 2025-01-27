3f076b24ac4ad4f2f74df620ad3c4870366cf0b5
Rohit mhetre <rohitmhetre2004@gmail.com>
2025-01-27 12:13:35 +0530

    Update ordercontroler.js

--- backend/controllers/ordercontroler.js
+++ backend/controllers/ordercontroler.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placeing user order for frontend
 const placeOrder = async(req,res) => {
 
-    const frontend_url ="http://localhost:5173";
+    const frontend_url ="https://foodflow-ui.onrender.com";
 
     try{
       const newOrder = new orderModel({
